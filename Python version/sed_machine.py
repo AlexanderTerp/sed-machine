982d2225cec915865fc55058f98483a2e35ed0a6
Terpal47 <alexander.terp@gmail.com>
2016-05-26 20:27:50 +1000

    Added a Python version of the script

---
+++ Python version/sed_machine.py
@@ -0,0 +1,42 @@
+#------------------------------------------------------------------------------
+# Original author:  Terpal47
+# Creation date:    26/05/2016
+# Description:      Encrypts and decrypts messages based on a symmetric-key
+#                   encryption system.
+#------------------------------------------------------------------------------
+
+import os
+from random import choice
+
+CHARACTERS = """ AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890!@#$%^&*()_+.,;<>/:=|\\[]{}`~?-\'\" """
+
+def generate_key():
+    key = []
+    characters_remaining = list(CHARACTERS)
+    while characters_remaining:
+        key.append(choice(characters_remaining))
+        characters_remaining.remove(key[-1])
+    return "".join(key)
+
+def encrypt(message, key):
+    encrypted = ""
+    for character in message:
+        index = CHARACTERS.index(character)
+        encrypted += key[index]
+    return encrypted
+
+def decrypt(message, key):
+    decrypted = ""
+    for character in message:
+        index = key.index(character)
+        decrypted += CHARACTERS[index]
+    return decrypted
+
+key = generate_key()
+message = input()
+
+encrypted = encrypt(message, key)
+decrypted = decrypt(encrypted, key)
+
+print(encrypted)
+print(decrypted)
