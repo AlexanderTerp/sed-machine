7331dbf9a6c7208c80585e0ceec7250cefd4ebee
Terpal47 <alexander.terp@gmail.com>
2016-06-02 23:02:25 +1000

    Cleaned up GUI and code
    Stayed more consistent in the code in terms of placing methods used.
    Also moved things around such that the GUI is now symmetrical. Added an
    icon as well.

--- Python version/sed_machine.py
+++ Python version/sed_machine.py
@@ -48,43 +48,47 @@ def decrypt():
 root = Tk()
 
 # Sets window size
-root.geometry('600x400')
+root.geometry('600x350')
+# Makes it resizable only in the x-axis.
+root.resizable(1,0)
+# Set icon
+root.iconbitmap('icon.ico')
 
 app = Window(root)
 
 # Places the top, middle, and bottom frames.
 # Top frame contains generate key, encrypt, and decrypt buttons.
-top_frame = Frame(root, width=600, height=200)
-top_frame.place(x=0,y=0)
+top_frame = Frame(root)
+top_frame.pack(side='top')
 
 # Middle frame contains the key_entry field.
-mid_frame = Frame(root, width=600, height=200)
-mid_frame.place(x=0,y=100)
+mid_frame = Frame(root)
+mid_frame.pack()
 
 # Contains the text box for messages.
-bottom_frame = Frame(root, width=600, height=200)
-bottom_frame.place(x=0,y=150)
+bottom_frame = Frame(root)
+bottom_frame.pack(side='bottom', fill='x')
 
 # Creates and places the Generate Key button.
-generate_b = Button(top_frame, text='Generate Key', command=generate_key)
-generate_b.pack(side=LEFT, padx=20, pady=20)
+generate_b = Button(mid_frame, text='Generate Key', command=generate_key)
+generate_b.grid(row=0, columnspan=2, padx=20)
 
 # Creates and places the Encrypt button.
-encrypt_b = Button(top_frame, text='Encrypt!', command=encrypt)
-encrypt_b.pack(side=LEFT)
+encrypt_b = Button(mid_frame, text='Encrypt!', command=encrypt)
+encrypt_b.grid(row=1, column=0, padx=20, pady=20)
 
 # Creates and places the Decrypt button.
-decrypt_b = Button(top_frame, text='Decrypt!', command=decrypt)
-decrypt_b.pack(side=LEFT)
+decrypt_b = Button(mid_frame, text='Decrypt!', command=decrypt)
+decrypt_b.grid(row=1, column=1, padx=20, pady=20)
 
 # Creates and places the field for the key.
-key_entry = Entry(mid_frame, width=98)
-key_entry.pack(side=BOTTOM)
+key_entry = Entry(top_frame, width=98)
+key_entry.grid(pady=20)
 
 # Creates and places the text box for storing encrypted and un/decrypted
 # messages.
-message_text = Text(bottom_frame, width=64, height=12)
-message_text.grid(row=0, columnspan=2)
+message_text = Text(bottom_frame, height=12)
+message_text.pack(expand=1, fill='x')
 
 # Loops the window indefinitely.
 root.mainloop()
